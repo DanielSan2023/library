cc45fac334fb37b0c00241abebc2b045839f2c89
DanielSan2023 <electronic.san@gmail.com>
2025-05-16 16:29:57 +0200

    added docker-compose.yaml for config JDBC connection with Mysql container

--- src/main/java/com/example/library/mapper/ModelMapperConfig.java
+++ src/main/java/com/example/library/mapper/ModelMapperConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
         return new ModelMapper();
     }
 }
